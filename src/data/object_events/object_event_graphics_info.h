79efa5b5419af5e2b227f0c7055cd371f7dadb67
keremsama <201658364+keremsama@users.noreply.github.com>
2026-05-09 10:39:59 +0200

    Cutable Tree uses own Palette - Rustboro Door animation color fix

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -1539,12 +1539,12 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_UnusedPorygon2Doll
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_CuttableTree = {
     .tileTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_3,
+    .paletteTag = OBJ_EVENT_PAL_TAG_HM_OBJECTS,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 128,
     .width = 16,
     .height = 16,
-    .paletteSlot = PALSLOT_NPC_3,
+    .paletteSlot = PALSLOT_HM_OBJEKTS,
     .shadowSize = SHADOW_SIZE_S,
     .inanimate = TRUE,
     .compressed = FALSE,
