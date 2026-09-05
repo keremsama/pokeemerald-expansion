6ee8e2c0eaeab3e55ba68e8f65eae8a7906e6619
keremsama <201658364+keremsama@users.noreply.github.com>
2026-09-05 18:06:59 +0200

    small fix for Battle indicator

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -2633,6 +2633,9 @@ enum
 
 static bool32 ShouldShowTypeEffectiveness(u32 targetId)
 {
+    if (gBattleTypeFlags & BATTLE_TYPE_FRONTIER)
+        return TRUE;
+
     if (B_SHOW_EFFECTIVENESS == SHOW_EFFECTIVENESS_CAUGHT)
         return GetSetPokedexFlag(SpeciesToNationalPokedexNum(gBattleMons[targetId].species), FLAG_GET_CAUGHT);
 
