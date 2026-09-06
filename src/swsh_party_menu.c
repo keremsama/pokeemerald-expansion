8cdbf62cd6b2514a9e2df142ad42b3dd7c40189c
keremsama <201658364+keremsama@users.noreply.github.com>
2026-09-06 21:35:53 +0200

    Over Level 50's can be selected in Battle Frontier

--- src/swsh_party_menu.c
+++ src/swsh_party_menu.c
@@ -9038,8 +9038,6 @@ static u8 GetBattleEntryLevelCap(void)
     case FACILITY_UNION_ROOM:
         return UNION_ROOM_MAX_LEVEL;
     default: // Battle Frontier
-        if (gSpecialVar_0x8004 == FRONTIER_LVL_50)
-            return FRONTIER_MAX_LEVEL_50;
         return FRONTIER_MAX_LEVEL_OPEN;
     }
 }
